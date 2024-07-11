f87d0c01348f7e2ca6c22d19b9642d6b872be075
unofficialabj <unofficial.abj@gmail.com>
2024-07-11 10:02:14 +0545

    export fixed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export { default as HomeIcon } from "./icons/HomeIcon";
-export { default as Plus } from "./icons/HomeIcon";
+export { default as ArchiveIcon } from "./Linear/Money/Archive";
+export { default as CardRemoveIcon } from "./Linear/Money/Card-Remove";
+export { default as CardTick } from "./Linear/Money/Card-Tick";
 export type { IconProps } from "./types";
+export { ThemeProvider, useTheme } from "./ThemeProvider";
